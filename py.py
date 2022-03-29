6e8f583365379301d711921d3a512a53e07a58e3
shashichawan <102402158+shashichawan@users.noreply.github.com>
2022-03-29 10:04:03 +0530

    Add files via upload

---
+++ py.py
@@ -0,0 +1,19 @@
+#print("hello, world")#hello, world
+#print("hello world")#hello world
+#print("hello"+''+"world")#helloworld
+#print("hello""world",'core','python',sep="")#helloworldcorepython
+#print("hello","world",'core','python',sep="-")#hello-world-core-python
+#print("hello","world",'core','python',sep="-",)#hello-world-core-python
+#print("hello","world",'core','python',sep="-""ok")#hello-okworld-okcore-okpython
+#print("hello","world",'core','python',sep="-"''"ok")#hello-okworld-okcore-okpython
+
+# print("hello","python",end=".")#hellopython.hello-pythonnew version is 3.10.3ok fine
+# print("hello","python",end="-")
+# print("new version is 3.10.3")
+# print("ok fine")
+
+# print("happy","ugadi",end=".",sep='')
+# print("happy","ugadi",end=".",sep='-')
+# print("happy","ugadi",sep="@",ene="#")#happyugadi.happy-ugadi.happy@ugadi#
+
+#print("hello world")#hello worldd
